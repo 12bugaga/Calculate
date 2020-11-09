879298f851b95c27813c0e7d1db640124dbd43c2
Сергей <s12bugaga@gmail.com>
2020-11-09 22:01:51 +0300

    Change name param

---
+++ MainFunc/Calculate.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+
+namespace MainFunc
+{
+    public class Calculate
+    {
+        public const int SYMBOL = 0;
+        static int DefPriopity(char operation)
+        {
+            if (operation == '+' || operation == '-') return 1;
+            else if (operation == '*' || operation == '/') return 2;
+            else return 0;
+        }
+
+        static double Subtraction(double a, double b)
+        {
+            return (a - b);
+        }
+        static double Addition(double a, double b)
+        {
+            return (a + b);
+        }
+        static double Division(double a, double b)
+        {
+            return (a / b);
+        }
+        static double Multiplication(double a, double b)
+        {
+            return (a * b);
+        }
+        static void CalculateLastOperation(ref Stack<double> arguments, char operations)
+        {
+            double result = 0, b = arguments.Pop(), a=arguments.Pop();
+            switch  (operations)
+            {
+                case '-':
+                    result = Subtraction(a, b);
+                    break;
+                case '+':
+                    result = Addition(a, b);
+                    break;
+                case '*':
+                    result = Multiplication(a, b);
+                    break;
+                case '/':
+                    if (b == 0)
+                        throw new DivideByZeroException();
+                    result = Division(a, b);
+                    break;
+            }
+            arguments.Push(Math.Round(result, 2));
+        }
+
+        public static double CalculateExample(List<string> separateString)
+        {
+            double num;
+            Stack<double> parametrs = new Stack<double>();
+            Stack<char> actions = new Stack<char>();
+            foreach(string val in separateString)
+            {
+                //Добавляем число в стек
+                if (double.TryParse(val, out num))
+                    parametrs.Push(num);
+                //Добавляем откр скобку в стек
+                else if (val == "(")
+                    actions.Push(val.ToCharArray()[SYMBOL]);
+                //Вычисляем до момента пока не наткнёмся на откр скобку
+                else if (val == ")")
+                {
+                    while (actions.Peek() != '(' && parametrs.Count != 1)
+                    {
+                        CalculateLastOperation(ref parametrs, actions.Pop());
+                    }
+                    actions.Pop();
+                }
+                //Действия при получения операнда
+                else
+                {
+                    //Первый знак
+                    if (actions.Count == 0)
+                        actions.Push(val.ToCharArray()[SYMBOL]);
+                    //Знак меньший в приоритете
+                    else if (DefPriopity(val.ToCharArray()[SYMBOL]) <= (DefPriopity(actions.Peek())))
+                    {
+                        while ((actions.Count != 0 && (DefPriopity(val.ToCharArray()[SYMBOL]) <= DefPriopity(actions.Peek()))))
+                            CalculateLastOperation(ref parametrs, actions.Pop());
+                        actions.Push(val.ToCharArray()[SYMBOL]);
+                    }
+                    //Знак больший в приоритете
+                    else
+                        actions.Push(val.ToCharArray()[SYMBOL]);
+                }
+            }
+            //Вычисления, когда стек заполнен
+            while (actions.Count != 0)
+                CalculateLastOperation(ref parametrs, actions.Pop());
+            return parametrs.Pop();
+        }
+    }
+}
--- MainFunc/ProcessingFirstStr.cs
+++ MainFunc/ProcessingFirstStr.cs
@@ -34,17 +34,20 @@ namespace MainFunc
                     closeCount++;
             }
             if (openCount != closeCount)
-                throw new Exception("Неверно расставлены скобки!уч");
+                throw new Exception("Неверно расставлены скобки!");
         }
 
         public static List<string>SeparationText(string firstStr)
         {
+            int previous = 0, next = 0;
             if (String.IsNullOrEmpty(firstStr))
                     throw new ArgumentNullException("firstStr");
             string value="";
             List<string> finishStr = new List<string>();
             for (int num = 0; num < firstStr.Length; num++)
             {
+                previous = num - 1;
+                next = num + 1;
                 if (char.IsDigit(firstStr[num]))
                     value += firstStr[num];
                 else
@@ -57,7 +60,7 @@ namespace MainFunc
                             finishStr.Add(firstStr[num].ToString());
                         }
                         //Скобка после знака
-                        else if (firstStr[num] == '(' && !char.IsDigit(firstStr[num - 1]))
+                        else if (firstStr[num] == '(' && !char.IsDigit(firstStr[previous]))
                         {
                             finishStr.Add(firstStr[num].ToString());
                         }
@@ -70,7 +73,7 @@ namespace MainFunc
                             finishStr.Add(firstStr[num].ToString());
                         }
                         //Закрывающаяся скобка перед числом => умножение
-                        else if (firstStr[num] == ')' && num + 1 != firstStr.Length && char.IsDigit(firstStr[num + 1]))
+                        else if (firstStr[num] == ')' && next != firstStr.Length && char.IsDigit(firstStr[next]))
                         {
                             finishStr.Add(value);
                             value = "";
@@ -85,13 +88,13 @@ namespace MainFunc
                             finishStr.Add(firstStr[num].ToString());
                         }
                         //Если отрицательный знак стоит в начале примера или после откр скобки
-                        else if ((num == 0 && value == "") || (firstStr[num - 1] == '('))
+                        else if ((num == 0 && value == "") || (firstStr[previous] == '('))
                         {
                             finishStr.Add("0");
                             finishStr.Add(firstStr[num].ToString());
                         }
                         //Знак стоит после закр скобки
-                        else if (firstStr[num - 1] == ')')
+                        else if (firstStr[previous] == ')')
                             finishStr.Add(firstStr[num].ToString());
                         //Если знак стоит после числа
                         else if (value != "")
@@ -101,7 +104,7 @@ namespace MainFunc
                             value = "";
                         }
                         //Два знака стоят рядом без скобок
-                        else if (AvailableOperation(firstStr[num]) && ((finishStr != null) || (value != "")) && !char.IsDigit(firstStr[num - 1]))
+                        else if (AvailableOperation(firstStr[num]) && ((finishStr != null) || (value != "")) && !char.IsDigit(firstStr[previous]))
                             throw new Exception("Неверно расставлены знаки операций!");
                     }
                 }
@@ -111,101 +114,6 @@ namespace MainFunc
             }
             CheckForBracket(finishStr);
             return finishStr;
-        }
-    }
-
-    public class Calculate
-    {
-        static int DefPriopity(char operation)
-        {
-            if (operation == '+' || operation == '-') return 1;
-            else if (operation == '*' || operation == '/') return 2;
-            else return 0;
-        }
-
-        static double Subtraction(double a, double b)
-        {
-            return (a - b);
-        }
-        static double Addition(double a, double b)
-        {
-            return (a + b);
-        }
-        static double Division(double a, double b)
-        {
-            return (a / b);
-        }
-        static double Multiplication(double a, double b)
-        {
-            return (a * b);
-        }
-        static void CalculateLastOperation(ref Stack<double> arguments, char operations)
-        {
-            double result = 0, b = arguments.Pop(), a=arguments.Pop();
-            switch  (operations)
-            {
-                case '-':
-                    result = Subtraction(a, b);
-                    break;
-                case '+':
-                    result = Addition(a, b);
-                    break;
-                case '*':
-                    result = Multiplication(a, b);
-                    break;
-                case '/':
-                    if (b == 0)
-                        throw new DivideByZeroException();
-                    result = Division(a, b);
-                    break;
-            }
-            arguments.Push(Math.Round(result, 2));
-        }
-
-        public static double CalculateExample(List<string> separateString)
-        {
-            double num;
-            Stack<double> parametrs = new Stack<double>();
-            Stack<char> actions = new Stack<char>();
-            foreach(string val in separateString)
-            {
-                //Добавляем число в стек
-                if (double.TryParse(val, out num))
-                    parametrs.Push(num);
-                //Добавляем откр скобку в стек
-                else if (val == "(")
-                    actions.Push(val.ToCharArray()[0]);
-                //Вычисляем до момента пока не наткнёмся на откр скобку
-                else if (val == ")")
-                {
-                    while (actions.Peek() != '(' && parametrs.Count != 1)
-                    {
-                        CalculateLastOperation(ref parametrs, actions.Pop());
-                    }
-                    actions.Pop();
-                }
-                //Действия при получения операнда
-                else
-                {
-                    //Первый знак
-                    if (actions.Count == 0)
-                        actions.Push(val.ToCharArray()[0]);
-                    //Знак меньший в приоритете
-                    else if (DefPriopity(val.ToCharArray()[0]) <= (DefPriopity(actions.Peek())))
-                    {
-                        while ((actions.Count != 0 && (DefPriopity(val.ToCharArray()[0]) <= DefPriopity(actions.Peek()))))
-                            CalculateLastOperation(ref parametrs, actions.Pop());
-                        actions.Push(val.ToCharArray()[0]);
-                    }
-                    //Знак больший в приоритете
-                    else
-                        actions.Push(val.ToCharArray()[0]);
-                }
-            }
-            //Вычисления, когда стек заполнен
-            while (actions.Count != 0)
-                CalculateLastOperation(ref parametrs, actions.Pop());
-            return parametrs.Pop();
         }
     }
 }
